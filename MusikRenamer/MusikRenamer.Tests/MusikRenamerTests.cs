5a9008ccda0308e83a253724aa5e7fe0dd1aa1bd
Eli Brighton <st3gs@hotmail.com>
2017-09-09 23:52:06 +1000

    Add unit test project

---
+++ MusikRenamer/MusikRenamer.Tests/MusikRenamerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MusikRenamer.Tests
+{
+    [TestClass]
+    public class MusikRenamerTests
+    {
+        //[TestMethod]
+        //public void GetUnprocessedDirectory_DirectoryFound_Test()
+        //{
+        //    // Arrange
+        //    var expectedUnprocessedDirectory = @"C:\DJ Eli B\Unprocessed";
+
+        //    // Act
+
+        //    // Assert
+        //}
+    }
+}
---
+++ MusikRenamer/MusikRenamer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MusikRenamer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MusikRenamer.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("87c214a5-ac1d-46e8-8fbf-8e2cbc50fa1b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
